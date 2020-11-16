6a4f64b16ec1b39a75bcb8c5f5fe27ab137716b5
Harry Kelly <73826459+hk944@users.noreply.github.com>
2020-11-16 14:35:00 +0000

    Added camera and set default resolution to 1280*720p

--- core/src/com/zanetta/auber/ProjectAuberGame.java
+++ core/src/com/zanetta/auber/ProjectAuberGame.java
@@ -3,22 +3,25 @@ package com.zanetta.auber;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class ProjectAuberGame extends ApplicationAdapter {
+	OrthographicCamera camera;
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
+		camera = new OrthographicCamera(1280, 720);
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
