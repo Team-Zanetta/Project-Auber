5d9566662a552e9ea23bb4415baa63c387d7ca6d
Harry Kelly <73826459+hk944@users.noreply.github.com>
2020-11-16 14:22:25 +0000

    Default LibGDX Setup

---
+++ desktop/src/com/zanetta/auber/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.zanetta.auber.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.zanetta.auber.ProjectAuberGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProjectAuberGame(), config);
+	}
+}
