2a7cfae1e10a4e8cf51a39856dcbab8554d3da64
Harry Kelly <73826459+hk944@users.noreply.github.com>
2020-11-18 14:22:00 +0000

    Created textures class
    Wrapper for texture atlas, make sure to import textures when setting up game - find all textures by passing texture name to this class

---
+++ core/src/com/zanetta/auber/Textures.java
@@ -0,0 +1,22 @@
+package com.zanetta.auber;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+
+public class Textures {
+	
+	private static TextureAtlas atlas;
+	
+	public static void importTextures(String filepath) {
+		atlas = new TextureAtlas(Gdx.files.internal(filepath));
+	}
+	
+	public static AtlasRegion getTexture(String textureName) {
+		return atlas.findRegion(textureName);
+	}
+	
+	public void dispose() {
+		atlas.dispose();
+	}
+}
