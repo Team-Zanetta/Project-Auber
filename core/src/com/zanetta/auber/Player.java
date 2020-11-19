89231faf9a32e983aca4c1ea2b9fc80e6c1c2739
Harry Kelly <73826459+hk944@users.noreply.github.com>
2020-11-19 15:29:32 +0000

    Revert "Merge branch 'Player-Infiltrator-Interraction-(Harry)' into ALexnadre"
    This reverts commit d8d41d1f808d9c11a03adada0e5e1bb135a6c4c5, reversing
    changes made to 3d6a529e15f1db70da8d1ec45ecbf9b6287b8896.

--- core/src/com/zanetta/auber/Player.java
+++ core/src/com/zanetta/auber/Player.java
@@ -1,99 +1,41 @@
 package com.zanetta.auber;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.actions.MoveByAction;
-import com.badlogic.gdx.utils.Array;
 
 
-public class Player extends Actor implements Sprite, InputProcessor{
-	private TextureRegion textureRegion;
-	private float xVelocity, yVelocity;
-	private float movementDistance = 2;
-	private float movementTime = 0.1f;
-	private float scannerSlowdown = 0.5f;
-	public boolean scanning;
-	private float scannerRadius = 500;
+public class Player extends Actor{
+    private TextureRegion region;
 
-	
-	public  Player(TextureRegion textureRegion){
+
+    public Player(TextureRegion region){
         super();
-//        Sets up textures
-        this.textureRegion = textureRegion;
-        setSize(this.textureRegion.getRegionWidth(), this.textureRegion.getRegionHeight());
-//        Sets up default velocities
-        xVelocity = 0;
-        yVelocity = 0;
-//        Sets up input
-        Gdx.input.setInputProcessor(this);
-        
+        this.region = region;
+        setSize(this.region.getRegionWidth(), this.region.getRegionHeight());
+    }
+
+
+    public TextureRegion getRegion(){
+        return region;
+    }
+
+
+    public void setRegion(TextureRegion region){
+        this.region = region;
+        setSize(this.region.getRegionWidth(), this.region.getRegionHeight());
     }
 
     @Override
     public void act(float delta) {
-    	
-//    	Creates movement actions if the keys are pressed
-    	if(xVelocity != 0 || yVelocity != 0) {
-    		float factor = movementDistance;
-        	if(scanning) {
-        		factor = factor * scannerSlowdown;
-        	}
-        	
-    		MoveByAction moveAction = new MoveByAction();
-            moveAction.setAmount(xVelocity * factor, yVelocity * factor);;
-            moveAction.setDuration(movementTime);
-            this.addAction(moveAction);
-    	}
-    	
-    	if(scanning) {
-    		scan();
-    	}
-    	
-//    	Executes actions
-    	super.act(delta);
-    }
-    
-    private void scan() {
-    	float [] playerLocation = getCentrePoint();
-    	Array<Actor> actors = this.getStage().getActors();
-    	for (Actor actor : actors){
-    		if(actor instanceof Infiltrator) {
-    			Infiltrator infiltrator = (Infiltrator) actor;
-    			if(!infiltrator.getHasBeenScanned()) {
-	    			float [] infiltratorLocation = infiltrator.getCentrePoint();
-	    			
-	    			float dx = Math.abs(playerLocation[0] - infiltratorLocation[0]);
-	    			float dy = Math.abs(playerLocation[1] - infiltratorLocation[1]);
-	    			
-//    				Quick square check
-	    			if(dx < scannerRadius & dy < scannerRadius) {
-//    					Slower circle check
-	    				if(Math.sqrt(dx*dx+dy*dy) < scannerRadius) {
-	    					infiltrator.scan();
-	    				}
-	    			}
-    			}
-    		}
-    	}
-    }
-    
-    public float[] getCentrePoint() {
-    	float x = getX() + getWidth()/2;
-    	float y = getY() + getHeight()/2;
-    	return new float[] {x,y};
+        super.act(delta);
     }
 
 
-//    Draws the player
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
-
-        if (textureRegion == null | !isVisible()){
+        if (region == null || !isVisible()){
             return;
         }
 
@@ -112,10 +54,10 @@ public class Player extends Actor implements Sprite, InputProcessor{
                 getScaleX(), getScaleY(),
                 getRotation());
     }
-    
     public Infiltrator enemyCarrying(){
         return null;
     }
+
 
     public Enum state(){
 
@@ -125,95 +67,4 @@ public class Player extends Actor implements Sprite, InputProcessor{
     public void pickupDropEnemy(){
 
     }
-
-
-	@Override
-	public TextureRegion getTextureRegion() {
-		return textureRegion;
-	}
-
-
-	@Override
-	public void setTextureRegion(TextureRegion textureRegion) {
-		this.textureRegion = textureRegion;
-        setSize(this.textureRegion.getRegionWidth(), this.textureRegion.getRegionHeight());
-		
-	}
-
-	@Override
-	public boolean keyDown(int keycode) {
-//		Starts the scanner actions on next act if space is pressed
-		if(keycode == Keys.SPACE) {
-			scanning = true;
-		}
-		
-//		Sets the relative velocities on button presses 
-		if(keycode == Keys.RIGHT | keycode == Keys.D) {
-			xVelocity += 1;
-		}if(keycode == Keys.LEFT | keycode == Keys.A) {
-			xVelocity -= 1;
-		}if(keycode == Keys.UP | keycode == Keys.W) {
-			yVelocity += 1;
-		}if(keycode == Keys.DOWN | keycode == Keys.S) {
-			yVelocity -= 1;
-		}
-		return true;
-	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-//		Ends scanning on space up
-		if(keycode == Keys.SPACE) {
-			scanning = false;
-		}
-		
-//		Reduces the relative velocities on button up
-		if(keycode == Keys.RIGHT | keycode == Keys.D) {
-			xVelocity += -1;
-		}if(keycode == Keys.LEFT | keycode == Keys.A) {
-			xVelocity -= -1;
-		}if(keycode == Keys.UP | keycode == Keys.W) {
-			yVelocity += -1;
-		}if(keycode == Keys.DOWN | keycode == Keys.S) {
-			yVelocity -= -1;
-		}
-			
-		return true;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(float amountX, float amountY) {
-		// TODO Auto-generated method stub
-		return false;
-	}
 }
--- core/src/com/zanetta/auber/ProjectAuberGame.java
+++ core/src/com/zanetta/auber/ProjectAuberGame.java
@@ -4,37 +4,33 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class ProjectAuberGame extends ApplicationAdapter {
 	OrthographicCamera camera;
-	Stage stage;
-	Player player;
+	SpriteBatch batch;
+	Texture img;
 	
 	@Override
 	public void create () {
 		camera = new OrthographicCamera(1280, 720);
-		Textures.importTextures("Auber pack.atlas");
-		
-		stage = new Stage();
-		player = new Player(Textures.getTexture("Player"));
-		Infiltrator infiltrator = new Infiltrator(Textures.getTexture("Enemy"));
-		stage.addActor(player);
-		stage.addActor(infiltrator);
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		stage.act(Gdx.graphics.getDeltaTime());
-		stage.draw();
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		Textures.dispose();
-		stage.dispose();
+		batch.dispose();
+		img.dispose();
 	}
 }
