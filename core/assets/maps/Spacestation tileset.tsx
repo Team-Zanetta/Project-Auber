db16e68ac5779ecf506923913bb4333af92a8deb
tdkc500 <73826380+tdkc500@users.noreply.github.com>
2020-11-20 17:14:12 +0000

    Add files via upload

---
+++ core/assets/maps/Spacestation tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Spacestation tileset" tilewidth="32" tileheight="32" tilecount="220" columns="20">
+ <image source="spacestation tileset.jpg" width="640" height="361"/>
+</tileset>
