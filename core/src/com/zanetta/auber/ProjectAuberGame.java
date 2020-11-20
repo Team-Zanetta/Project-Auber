4b25163ea74bd64d06db69ab894bd94e638de52c
Harry Kelly <73826459+hk944@users.noreply.github.com>
2020-11-20 17:09:32 +0000

    Revert "Merge branch 'master' into Harry"
    This reverts commit bfbca33bd0fde61a8b17cddf1059b5e5ea792b8b, reversing
    changes made to 21c24453a9a27dd95002b967b38f3d39326a6668.

--- core/src/com/zanetta/auber/ProjectAuberGame.java
+++ core/src/com/zanetta/auber/ProjectAuberGame.java
@@ -1,54 +1,18 @@
 package com.zanetta.auber;
 
 import com.badlogic.gdx.ApplicationAdapter;
-
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.maps.MapProperties;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class ProjectAuberGame extends ApplicationAdapter {
-	
 	OrthographicCamera camera;
 	Stage stage;
 	
-	//Map
-	private TiledMap map;
-	private AssetManager manager;
-	private int tileWidth, tileHeight,
-    mapWidthInTiles, mapHeightInTiles,
-    mapWidthInPixels, mapHeightInPixels;
-	
-	// Render
-	private OrthogonalTiledMapRenderer renderer;
-	
 	@Override
 	public void create () {
-		manager = new AssetManager();
-		manager.setLoader(TiledMap.class, new TmxMapLoader());
-		manager.load("maps/Spaceship.tmx", TiledMap.class);
-        manager.finishLoading();
- 
-        map = manager.get("maps/Spaceship.tmx", TiledMap.class);
-        
-        // Read properties
-        MapProperties properties = map.getProperties();
-        tileWidth         = properties.get("tilewidth", Integer.class);
-        tileHeight        = properties.get("tileheight", Integer.class);
-        mapWidthInTiles   = properties.get("width", Integer.class);
-        mapHeightInTiles  = properties.get("height", Integer.class);
-        mapWidthInPixels  = mapWidthInTiles  * tileWidth;
-        mapHeightInPixels = mapHeightInTiles * tileHeight;
-		
 		camera = new OrthographicCamera(1280, 720);
-        camera.position.x = mapWidthInPixels * .5f;
-        camera.position.y = mapHeightInPixels * .35f;
-        
 		Textures.importTextures("Auber pack.atlas");
 		
 		stage = new Stage();
@@ -71,7 +35,6 @@ public class ProjectAuberGame extends ApplicationAdapter {
 	
 	@Override
 	public void dispose () {
-		manager.dispose();
 		Textures.dispose();
 		stage.dispose();
 	}
