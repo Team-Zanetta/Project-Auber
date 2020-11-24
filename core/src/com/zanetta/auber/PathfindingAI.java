fc365c456dccccd9de9ffe0bc4ebed0439826e26
sm2447 <73826426+sm2447@users.noreply.github.com>
2020-11-24 16:29:13 +0000

    Update PathfindingAI.java

--- core/src/com/zanetta/auber/PathfindingAI.java
+++ core/src/com/zanetta/auber/PathfindingAI.java
@@ -1,17 +1,5 @@
 package com.zanetta.auber;
 
-public class Node {
-	public int i, j;
-	public int heuristic_cost;
-	public int finalCost;
-	public boolean solution;
-	
-	public Node(int i, int j) {
-		this.i = i;
-		this.j = j;
-	}
-}
-
 public class PathFinding {
 	
 	
