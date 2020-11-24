7c912c5e84d7e991f12f406545c3d8ccafa25ca1
sm2447 <73826426+sm2447@users.noreply.github.com>
2020-11-24 16:28:43 +0000

    Create Node.java

---
+++ core/src/com/zanetta/auber/Node.java
@@ -0,0 +1,13 @@
+package com.zanetta.auber;
+
+public class Node {
+	public int i, j;
+	public int heuristic_cost;
+	public int finalCost;
+	public boolean solution;
+	
+	public Node(int i, int j) {
+		this.i = i;
+		this.j = j;
+	}
+}
