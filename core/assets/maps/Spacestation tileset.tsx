35bbea9f49780406c179c4b7339fa5ff688c6db7
Harry Kelly <73826459+hk944@users.noreply.github.com>
2020-11-25 09:18:23 +0000

    Added spacestation tileset

--- core/assets/maps/Spacestation tileset.tsx
+++ core/assets/maps/Spacestation tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Spacestation tileset" tilewidth="32" tileheight="32" tilecount="220" columns="20">
- <image source="spacestation tileset.jpg" width="640" height="361"/>
+<tileset version="1.4" tiledversion="1.4.3" name="Space-station._64" tilewidth="32" tileheight="32" tilecount="5712" columns="84">
+ <image source="Space-station._64.png" width="2688" height="2176"/>
 </tileset>
